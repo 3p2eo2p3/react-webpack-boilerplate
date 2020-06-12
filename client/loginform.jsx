29dcaea347035f7ac67873d794ea8a1747fc84d1
3p2eo2p3 <64085025+3p2eo2p3@users.noreply.github.com>
2020-06-12 20:45:41 +0500

    remove axios

--- client/loginform.jsx
+++ client/loginform.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux'
 
 class LoginForm extends React.Component {
@@ -20,20 +19,7 @@ class LoginForm extends React.Component {
   }
 
   async handleSubmit(event) {
-    console.log('Имя:   ' + this.state.value1 + 'Пароль:   ' + this.state.value2);
-    event.preventDefault();
-    axios.post('/api/login', {
-      name: this.state.value1,
-      password: this.state.value2
-    }).then((res) => {
-                this.state.loginstatus = res.data;
-                if (this.state.loginstatus == '1') {
-                localStorage.setItem('loginstatus', this.state.loginstatus);
-                this.props.setUserAction({user: this.state.value1}); ////REFERS TO REDUX >>>>>>>
-                window.location = "/home";
-              }
-               else {this.setState({errortext: 'Не удалось войти. Попробуйте снова или зарегестрируйтесь'})}
-              })
+   alert('Имя:   ' + this.state.value1 + 'Пароль:   ' + this.state.value2);
   }
 
   render() {
